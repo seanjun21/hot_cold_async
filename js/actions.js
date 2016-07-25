dbdeae9f2c536aae1fd91df5852a6c0949c86560
Robby Kim <robert.st.kim@gmail.com>
2016-07-25 16:35:02 -0400

    added newgame reducer and makeguess reducer

--- js/actions.js
+++ js/actions.js
@@ -1,4 +1,3 @@
-
 // ACTIONS are like messengers telling REDUCERS (are like event handlers) what action to be done
 // Only create ACTIONS that will attach to event listeners
 
@@ -25,5 +24,11 @@ var makeGuess = function(guess) {
   };
 };
 
+exports.newGame = newGame;
+exports.what = what;
+exports.makeGuess = makeGuess;
+exports.NEW_GAME = NEW_GAME;
+exports.WHAT = WHAT;
+exports.MAKE_GUESS = MAKE_GUESS;
 
 // Use Ref for input field
---
+++ js/reducers.js
@@ -0,0 +1,59 @@
+var actions = require('./actions');
+
+var hotColdReducer = function(state, action) {
+  state = state || {};
+
+  if (action.type === actions.NEW_GAME) {
+    var randNum = Math.floor((Math.random() * 100) + 1);
+    return {
+      correct: randNum,
+      guess: null,
+      counter: 0,
+      hotness: null
+    };
+  }
+
+  if (action.type === actions.MAKE_GUESS) {
+    if (action.guess > 0 && action.guess <= 100) {
+      var difference = Math.abs(state[0].correct - action.guess);
+      var hotness;
+
+      if (difference > 50) {
+        hotness = 'Super Cold';
+      } else if (difference > 30) {
+        hotness = 'Cold';
+      } else if (difference > 15) {
+        hotness = 'Warm';
+      } else if (difference > 1) {
+        hotness = 'Hot';
+      } else {
+        hotness = 'Got it!';
+      }
+
+      return Object.assign({}, state, {
+        guess: action.guess,
+        counter: state.counter + 1,
+        hotness: hotness
+      });
+    }
+  }
+  return state;
+};
+
+exports.hotColdReducer = hotColdReducer;
+
+
+// var MAKE_GUESS = 'MAKE_GUESS';
+// var makeGuess = function(guess) {
+//   return {
+//     type: MAKE_GUESS,
+//     guess: guess
+//   };
+// };
+
+// var NEW_GAME = 'NEW_GAME';
+// var newGame = function() {
+//   return {
+//     type: NEW_GAME
+//   };
+// };
